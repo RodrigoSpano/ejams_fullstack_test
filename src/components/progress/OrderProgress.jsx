22b000eadcddac75b5e4437b0da42a5d11f69eee
RodrigoSpano <rorrospano17@gmail.com>
2023-09-19 20:29:47 -0300

    progressbar responsive

--- src/components/progress/OrderProgress.jsx
+++ src/components/progress/OrderProgress.jsx
@@ -3,7 +3,7 @@ import ProgressItem from "./subcomps/ProgressItem"
 
 const OrderProgress = () => {
   return (
-    <div className='xl:w-[1245px] h-[40px] flex justify-between self-center mt-10 gap-3 min:gap-5 md:gap-10'>
+    <div className='xl:w-[1245px] h-[40px] flex justify-between self-center mt-10 gap-3 min:gap-5 md:gap-10 select-none'>
       {
         progressItemsHelper?.map((el, i) => <ProgressItem data={el} key={i} />)
       }
