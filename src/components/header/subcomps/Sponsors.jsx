a8e3c06d31b6059a48b0d17ba5b658fcc3eacd43
RodrigoSpano <rorrospano17@gmail.com>
2023-09-19 19:39:56 -0300

    sponsors responsive done

--- src/components/header/subcomps/Sponsors.jsx
+++ src/components/header/subcomps/Sponsors.jsx
@@ -2,11 +2,11 @@
 
 const Sponsors = () => {
   return (
-    <div className='flex justify-between items-center px-56 h-full select-none'>
-      <img src='/assets/logos/clarifion.png' alt='clarifion' className='h-[36px] w-[192px]'/>
-      <div className='flex gap-3 items-center justify-center h-[32px]'>
-        <img src='/assets/logos/mcafee.png' />
-        <img src='/assets/logos/norton.png' />
+    <div className='flex justify-between items-center px-10 lg:px-20 xl:px-56 h-full select-none'>
+      <img src='/assets/logos/clarifion.png' alt='clarifion' className='h-5 lg:h-[36px] w-[106.67px] lg:w-[192px]'/>
+      <div className='flex gap-3 items-center justify-center'>
+        <img className="h-4 md:h-[32px]" src='/assets/logos/mcafee.png' />
+        <img className="h-4 md:h-[32px]" src='/assets/logos/norton.png' />
       </div>
     </div>
   )
