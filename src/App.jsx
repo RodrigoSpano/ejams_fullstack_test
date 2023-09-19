78310ca0fc4a75d7f09a398d4079efcb2b86b747
RodrigoSpano <rorrospano17@gmail.com>
2023-09-19 18:14:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbarComps/Navbar"
+
+function App() {
+  return (
+    <div className="min-h-screen w-full flex flex-col">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbarComps/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[50px] w-full bg-[#252F3D]'>Navbar</div>
+  )
+}
+
+export default Navbar
