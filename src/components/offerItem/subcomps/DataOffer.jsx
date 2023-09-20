d50bb95ba9db55de8a59fdc87669d198f1e4bf04
RodrigoSpano <rorrospano17@gmail.com>
2023-09-20 11:28:20 -0300

    done

--- src/components/offerItem/subcomps/DataOffer.jsx
+++ src/components/offerItem/subcomps/DataOffer.jsx
@@ -1,17 +1,19 @@
 import Discount from "./Discount"
-import DiscountContainer from "./DiscountContainer"
+import DiscountContainer from "./discount/DiscountContainer"
 import Features from "./Features"
+import Guarantee from "./guarantee/Guarantee"
 import ItemInfo from "./ItemInfo"
 import OfferTitle from "./OfferTitle"
 
 const DataOffer = () => {
   return (
-    <div className="w-[550px] flex flex-col gap-5">
+    <div className="w-[550px] flex flex-col gap-7">
       <OfferTitle />
       <ItemInfo />
       <Features />
       <Discount />
       <DiscountContainer />
+      <Guarantee />
     </div>
   )
 }
---
+++ src/components/offerItem/subcomps/guarantee/Guarantee.jsx
@@ -0,0 +1,13 @@
+
+const Guarantee = () => {
+  return (
+    <div className="flex gap-4">
+      <img alt="guarantee" src="/assets/others/guarantee.png" className="object-contain" width={88} height={88}/>
+      <p className="text-[#4D5254]">
+      If you are not completely thrilled with your Clarifion - We have a <span className="font-bold">30 day satisfaction guarantee</span>. Please refer to our return policy at the bottom of the page for more details. Happy Shopping!
+      </p>
+    </div>
+  )
+}
+
+export default Guarantee
